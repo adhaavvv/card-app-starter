e9e72490bcbd8089796b5c8d3ff06e9ae20ed3d4
andytaotest <andytaotest@gmail.com>
2026-01-13 10:58:42 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import CardList from "./pages/CardList";
+import AddCard from "./pages/AddCard";
+import EditCard from "./pages/EditCard";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cards" element={<CardList />} />
+        {/* TODO: Complete the routes */}
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
