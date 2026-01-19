0ebf167b342621f7b937e3aba605e79d188e50a5
BeefCupine <brrbigmango@gmail.com>
2026-01-19 10:36:58 +0800

    Added Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,32 @@
 import { Link } from "react-router-dom";
 
 export default function Home() {
-  /* TODO: Design and complete the Home page
+    /* TODO: Design and complete the Home page
     - display instructions
     - link to Cards page
     - style as a landing page */
-  return <main></main>;
+
+  return (
+    <main>
+      <h1>Welcome to Card App by cmi</h1>
+      
+      <section>
+        <h2>Instructions:</h2>
+        <ol>
+          <li>Go to "Card List" to view all your cards</li>
+          <li>Go to "Add Card" to create new cards</li>
+          <li>Use the navbar to navigate between pages</li>
+        </ol>
+      </section>
+      
+      <section>
+        <h2>Quick Links:</h2>
+        <div>
+          <Link to="/cards">
+            <button>View Card List</button>
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
 }
-  
