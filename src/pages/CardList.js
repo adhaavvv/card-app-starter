c60644a3efdb499f6506f71aba213e1965489f98
adhav <adhavanbalagan@gmail.com>
2026-01-20 09:18:25 +0800

    changes to CardList

--- src/pages/CardList.js
+++ src/pages/CardList.js
@@ -3,11 +3,51 @@ import Card from "../components/Card";
 import { getCards, deleteCard } from "../services/api";
 
 export default function CardList() {
-  /* TODO: Complete the CardList page
-    - display a list of cards (use the Card component to display each card)
-    - delete button calling handleDelete with the card object
-    - handle loading, busy, and error states
-    - style as a grid UI */
 
-  return <main></main>;
+  const [cards, setCards] = useState([]);
+  const [error, setError] = useState("");
+  const [busy, setBusy] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  async function load() {
+    try {
+      const data = await getCards();
+      setCards(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  async function handleDelete(card) {
+    setBusy(true);
+    try {
+      await deleteCard(card.id);
+      setCards(cards.filter((c) => c.id !== card.id));
+    } catch (error) {
+      setError(error.message)
+    } finally {
+      setBusy(false);
+    }
+  }
+
+  return (
+  <main>
+    <h1>Card List</h1>
+    <div>
+      {cards.map((card) => (
+        <Card
+          key={card.id}
+          card={card}
+          onDelete={handleDelete}
+          disabled={busy}
+        />
+      ))}
+    </div>
+  </main>);
 }
